085ecc1ccbfa1a633d90cacc8fef79a8e69b78b5
Ninad More <ninadmore183@gmail.com>
2024-08-09 14:47:43 +0530

    Customers Who Never Order

---
+++ 0183-CustomersWhoNeverOrder.sql
@@ -0,0 +1,35 @@
+/*
+    Write a solution to find all customers who never order anything. Return the result table in any order.
+    The result format is in the following example.
+
+    Example 1:
+    Input: 
+    Customers table:
+    +----+-------+
+    | id | name  |
+    +----+-------+
+    | 1  | Joe   |
+    | 2  | Henry |
+    | 3  | Sam   |
+    | 4  | Max   |
+    +----+-------+
+    Orders table:
+    +----+------------+
+    | id | customerId |
+    +----+------------+
+    | 1  | 3          |
+    | 2  | 1          |
+    +----+------------+
+    Output: 
+    +-----------+
+    | Customers |
+    +-----------+
+    | Henry     |
+    | Max       |
+    +-----------+
+*/
+
+SELECT name AS Customers
+FROM Customers c
+WHERE c.id 
+NOT IN (SELECT customerId FROM Orders)
