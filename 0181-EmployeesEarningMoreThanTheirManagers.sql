8ca1e40b7d84028f220662c80f14d3d9923ef6f2
Ninad More <ninadmore183@gmail.com>
2024-08-09 14:40:35 +0530

    Employees Earning More Than Their Managers

---
+++ 0181-EmployeesEarningMoreThanTheirManagers.sql
@@ -0,0 +1,27 @@
+/*
+    Write a solution to find the employees who earn more than their managers. Return the result table in any order.
+    The result format is in the following example.
+
+    Example 1:
+
+    Input: 
+    Employee table:
+    +----+-------+--------+-----------+
+    | id | name  | salary | managerId |
+    +----+-------+--------+-----------+
+    | 1  | Joe   | 70000  | 3         |
+    | 2  | Henry | 80000  | 4         |
+    | 3  | Sam   | 60000  | Null      |
+    | 4  | Max   | 90000  | Null      |
+    +----+-------+--------+-----------+
+    Output: 
+    +----------+
+    | Employee |
+    +----------+
+    | Joe      |
+    +----------+
+*/
+
+SELECT name AS Employee
+FROM Employee E1
+where E1.salary > (SELECT salary FROM Employee WHERE Employee.id = E1.managerId)
