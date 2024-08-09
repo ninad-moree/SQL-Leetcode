2bbfba36831d4c36ec0b5bbb88a24d982cf6a8f2
Ninad More <ninadmore183@gmail.com>
2024-08-09 14:59:03 +0530

    Rising Temperature

---
+++ 0197-RisingTemperature.sql
@@ -0,0 +1,31 @@
+/*
+    Write a solution to find all dates' Id with higher temperatures compared to its previous dates (yesterday). Return the result table in any order.
+
+    Example 1:
+
+    Input: 
+    Weather table:
+    +----+------------+-------------+
+    | id | recordDate | temperature |
+    +----+------------+-------------+
+    | 1  | 2015-01-01 | 10          |
+    | 2  | 2015-01-02 | 25          |
+    | 3  | 2015-01-03 | 20          |
+    | 4  | 2015-01-04 | 30          |
+    +----+------------+-------------+
+    Output: 
+    +----+
+    | id |
+    +----+
+    | 2  |
+    | 4  |
+    +----+
+    Explanation: 
+    In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+    In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+*/
+
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE datediff(w1.recordDate, w2.recordDate) = 1
+AND w1.temperature > w2.temperature
