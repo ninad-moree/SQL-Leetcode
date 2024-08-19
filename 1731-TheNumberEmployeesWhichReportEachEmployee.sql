469e009365ff257d4a004c1e6c16d2efa9fb1d3c
Ninad More <ninadmore183@gmail.com>
2024-08-19 17:37:34 +0530

    The Number of Employees Which Report to Each Employee

---
+++ 1731-TheNumberEmployeesWhichReportEachEmployee.sql
@@ -0,0 +1,31 @@
+/*
+    Write a solution to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the 
+    reports rounded to the nearest integer. Return the result table ordered by employee_id.
+
+    Example 1:
+    Input: 
+    Employees table:
+    +-------------+---------+------------+-----+
+    | employee_id | name    | reports_to | age |
+    +-------------+---------+------------+-----+
+    | 9           | Hercy   | null       | 43  |
+    | 6           | Alice   | 9          | 41  |
+    | 4           | Bob     | 9          | 36  |
+    | 2           | Winston | null       | 37  |
+    +-------------+---------+------------+-----+
+    Output: 
+    +-------------+-------+---------------+-------------+
+    | employee_id | name  | reports_count | average_age |
+    +-------------+-------+---------------+-------------+
+    | 9           | Hercy | 2             | 39          |
+    +-------------+-------+---------------+-------------+
+    Explanation: Hercy has 2 people report directly to him, Alice and Bob. Their average age is (41+36)/2 = 38.5, which is 39 after rounding it to the 
+    nearest integer.
+*/
+
+SELECT e1.employee_id, e1.name, count(e2.employee_id) AS reports_count, ROUND(AVG(e2.age)) AS average_age
+FROM Employees e1
+JOIN Employees e2
+ON e1.employee_id = e2.reports_to
+GROUP BY e1.employee_id, e1.name
+ORDER BY e1.employee_id
