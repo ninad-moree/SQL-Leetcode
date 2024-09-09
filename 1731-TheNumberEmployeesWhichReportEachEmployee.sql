90bbdb473d1879ec517fae1af29542fcf212b7a1
Ninad More <ninadmore183@gmail.com>
2024-09-09 15:57:21 +0530

    The Number of Employees Which Report to Each Employee

--- 1731-TheNumberEmployeesWhichReportEachEmployee.sql
+++ 1731-TheNumberEmployeesWhichReportEachEmployee.sql
@@ -23,9 +23,12 @@
     nearest integer.
 */
 
-SELECT e1.employee_id, e1.name, count(e2.employee_id) AS reports_count, ROUND(AVG(e2.age)) AS average_age
+SELECT 
+    e1.employee_id, 
+    e1.name, 
+    COUNT(DISTINCT(e2.employee_id)) AS reports_count, 
+    ROUND(AVG(e2.age), 0) AS average_age
 FROM Employees e1
 JOIN Employees e2
 ON e1.employee_id = e2.reports_to
-GROUP BY e1.employee_id, e1.name
-ORDER BY e1.employee_id
+GROUP BY e1.employee_id
